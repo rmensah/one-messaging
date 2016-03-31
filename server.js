09cb89aa8fadeec993b4f2275c85ed54013a1fde
Alejandra Jurado <ajurado2204@gmail.com>
2016-03-31 17:58:12 -0400

    modified gitignore

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var bcrypt = require('bcryptjs');
 var expressSession = require('express-session');
 var mongoose = require('mongoose');
 var app = express();
-var PORT = process.env.PORT||3000;
+var PORT = process.env.PORT||3001;
 
 
 app.use(express.static(__dirname+"/public"));
