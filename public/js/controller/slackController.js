6d741b9fc2af03d57c1652abe96621f8c5c10f60
alemjc <alemjc2204@gmail.com>
2016-04-02 11:52:33 -0400

    controllers to implement were added.

---
+++ public/js/controller/slackController.js
@@ -0,0 +1,15 @@
+/**
+ * Created by alemjc on 4/2/16.
+ */
+var app = angular.module('messagingApp');
+
+
+/*
+ * userService is a service that is available to all controller. The service keeps user information and contains
+ * login and registration functions for the user. for more info look at userService.js
+ */
+app.controller('slackController', ['$userService', function($userService){
+
+
+
+}]);
