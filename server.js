e8b5325012ae911ed8257e1fc6f20aaf1f3460f9
alemjc <alemjc2204@gmail.com>
2016-04-03 15:31:49 -0400

    logging req params in /slackAuth

--- server.js
+++ server.js
@@ -202,7 +202,7 @@ app.post("/updateUser", function(req, res){
 app.get("/slackAuth", function(req, res){
 
   console.log("/slackAuth");
-  console.log(req.data);
+  console.log(req.params);
   res.redirect("/");
 
 });
