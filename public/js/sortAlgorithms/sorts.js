91fc5577915fb167e180b23783270039c6c8ca0d
alemjc <alemjc2204@gmail.com>
2016-04-09 20:30:08 -0400

    adding more debugging logs.

--- public/js/sortAlgorithms/sorts.js
+++ public/js/sortAlgorithms/sorts.js
@@ -8,12 +8,15 @@ var merge = function(arr1, arr2){
   var arr1Counter = 0;
   var arr2Counter = 0;
 
+  console.log("arr1.length: ",arr1.length);
+  console.log("arr2.length: ",arr2.length);
+
   while(arr1Counter < arr1.length || arr2Counter < arr2.length){
 
     if(arr1Counter < arr1.length && arr2Counter < arr2.length){
 
       if(arr1[arr1Counter].id.localeCompare(arr2[arr2Counter].id) < 0){
-        console.log("comparison is: ",arr1[arr1Counter].id.localeCompare(arr2[arr2Counter].id));
+        //console.log("comparison is: ",arr1[arr1Counter].id.localeCompare(arr2[arr2Counter].id));
         arr3.push(arr1[arr1Counter]);
         arr1Counter++;
       }
@@ -34,6 +37,9 @@ var merge = function(arr1, arr2){
 
   }
 
+  console.log("arr1Counter %d ==? arr1.length %d", arr1Counter,arr1.length);
+  console.log("arr2Counter %d ==? arr2.length %d", arr2Counter,arr2.length);
+
   return arr3;
 };
 
@@ -42,6 +48,7 @@ var mergeSortHelper = function(items, start, end){
   if((end - start)<2){
     //console.log("!!!!!!!!!!!!!!!!! reached base case !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     var arr = [];
+    console.log(start);
     arr.push(items[start]);
     return arr;
   }
