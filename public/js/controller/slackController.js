2ac1850361999c16b90fe1b8ec0e7db6bc45225e
alemjc <alemjc2204@gmail.com>
2016-04-10 00:19:29 -0400

    fixing injection error in slack controller.

--- public/js/controller/slackController.js
+++ public/js/controller/slackController.js
@@ -8,7 +8,7 @@ var app = angular.module('messagingApp');
  * userService is a service that is available to all controller. The service keeps user information and contains
  * login and registration functions for the user. for more info look at userService.js
  */
-app.controller('slackController', ['$userService', function($userService, Pusher){
+app.controller('slackController', ['$userService','Pusher', function($userService, Pusher){
     var slackC = this;
     Pusher.subscribe('slack','message', function(message){
         console.log("received message");
