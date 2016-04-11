06b66832b52ae49479bc6812ae0687732d259303
Alejandra Jurado <ajurado2204@gmail.com>
2016-04-10 23:21:45 -0400

    removed date

--- server.js
+++ server.js
@@ -413,7 +413,6 @@ function gmailMessagePull(req){
       var counter = 0;
       var len = response.messages.length;
       var messageArray = [];
-      var from;
       console.log(len);
       for(var i = 0; i < len; i++){
 
@@ -433,16 +432,10 @@ function gmailMessagePull(req){
               if(response.payload.headers[j].name === "From"){
                 console.log("FROM");
                 console.log(response.payload.headers[j].value);
-                from = response.payload.headers[j].value;
-              }
 
-              if(response.payload.headers[j].name === "Date"){
-                console.log("DATE");
-                console.log(response.payload.headers[j].value);
                 messageArray.push({
                   snippet: response.snippet,
-                  fromEmail: from,
-                  date: response.payload.headers[j].value
+                  fromEmail: response.payload.headers[j].value
                 });
               }
             }
