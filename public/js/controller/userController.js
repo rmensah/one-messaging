654675ead666adf3a96241ca7df5de67ca8089f8
Alejandra Jurado <ajurado2204@gmail.com>
2016-04-27 11:19:49 -0400

    trying to diaply username

--- public/js/controller/userController.js
+++ public/js/controller/userController.js
@@ -6,9 +6,11 @@ var app = angular.module('messagingApp');
 app.controller('userController', ['$userService','$http','$window','$location', function($userService, $http, $window, $location){
 
   var uC = this;
+  uC.username = $userService.user.username;
 
   uC.logOut = function(){
     $userService.logout();
   };
+
 
 }]);
